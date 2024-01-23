7715c6f71586370cfbe25e4ffce2bc1b533cb4fd
jintongxu <simon_yolo@163.com>
2024-01-23 15:50:50 +0800

    Uthread: switching between threads (moderate)

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,35 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
+
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context ctx;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context *old, struct context *new);
               
 void 
 thread_init(void)
@@ -63,6 +83,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->ctx, &next_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -77,6 +98,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->ctx.ra = (uint64)func;   // 返回地址
+  
+  // 将线程的栈指针指向其独立的栈,栈的生长方向是从高地址到低地址,所以要将 sp 设置为指向stack的高地址
+  t->ctx.sp = (uint64)&t->stack + (STACK_SIZE - 1);    // 栈指针
 }
 
 void 
